38e3a644fbb43934f338dc7bed05b1033ac3a732
romanyukalexandr84 <romanyukalexandr84@gmail.com>
2024-01-31 23:13:48 +0300

    add basic Server

---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Server extends JFrame {
+    private static final int POS_X = 400;
+    private static final int POS_Y = 350;
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 300;
+
+    private final JButton btnStart = new JButton("Запустить сервер");
+    private final JButton btnStop = new JButton("Остановить сервер");
+    private final JTextArea log = new JTextArea();
+    private boolean isServerWorking;
+
+    Server() {
+        isServerWorking = false;
+        btnStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                isServerWorking = true;
+                System.out.println("Сервер запущен");
+            }
+        });
+        btnStop.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                isServerWorking = false;
+                System.out.println("Сервер остановлен");
+            }
+        });
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBounds(POS_X, POS_Y, WIDTH, HEIGHT);
+        setResizable(false);
+        setTitle("Сервер чата");
+        setAlwaysOnTop(true);
+        setLayout(new GridLayout(1, 2));
+        add(btnStart);
+        add(btnStop);
+
+        setVisible(true);
+    }
+
+}
